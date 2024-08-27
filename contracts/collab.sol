fd71780fbbfe3e877a8b5ffbe0a24952ef5064c0
Injamam Ul Haque <131451388+Haqueinjam@users.noreply.github.com>
2024-08-27 17:04:27 +0530

    Update collab.sol

--- contracts/collab.sol
+++ contracts/collab.sol
@@ -1,1 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+contract AccessControl {
+    address public admin;
+    mapping(address => bool) public collaborators;
+
+    modifier onlyAdmin() {
+        require(msg.sender == admin, "Not an admin");
+        _;
+    }
+
+    modifier onlyCollaborator() {
+        require(collaborators[msg.sender], "Not a collaborator");
+        _;
+    }
+
+    constructor() {
+        admin = msg.sender;
+    }
+
+    function addCollaborator(address _collaborator) public onlyAdmin {
+        collaborators[_collaborator] = true;
+    }
+
+    function removeCollaborator(address _collaborator) public onlyAdmin {
+        collaborators[_collaborator] = false;
+    }
+}
